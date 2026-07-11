5856db47aaa4aec8e137fe071e736b8d8a182f3b
AADRIKA <aadrika1005@gmail.com>
2026-07-11 19:15:29 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown(
+        """
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;">Created by <b style="color:black;">Aadrika</b></p>
+
+        </div>
+        
+
+""",
+        unsafe_allow_html=True,
+    )
