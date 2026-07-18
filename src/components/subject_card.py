c51b30077e286a44b18f47e39ed06527d2c69680
AADRIKA <aadrika1005@gmail.com>
2026-07-18 11:08:59 +0530

    changes

--- src/components/subject_card.py
+++ src/components/subject_card.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-def subject_card(name, code, section, stats=None, footer_callback=None):
+def subject_card(name, code, section, stats=None):
     html = f"""
         <div style="background:white; border-left: 8px solid #EB459E; padding:25px; border-radius: 20px; border: 1px solid black; margin-bottom:20px;">
         <h3 style="margin:0; color: #1e293b; font-size: 1.5rem ">{name}</h3>
@@ -19,6 +19,3 @@ def subject_card(name, code, section, stats=None, footer_callback=None):
         html += "</div>"
 
     st.markdown(html, unsafe_allow_html=True)
-
-    if footer_callback:
-        footer_callback()
